b9fe24a7b77f648769106a2abdb5bbb4e1c6340b
vladimir <vladimirmuratov@mail.ru>
2025-02-05 11:36:09 +0300

    update title

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -38,9 +38,9 @@ export const Banner = () => {
                         xs: 'calc(var(--index) * 23)',
                         sm: 'calc(var(--index) * 23)',
                         md: 'calc(var(--index) * 23)',
-                        lg: 'calc(var(--index) * 20)'
+                        lg: 'calc(var(--index) * 23)'
                     },
-                    backgroundImage: `url(/images/images-for-landings/6.webp)`,
+                    backgroundImage: `url(/images/images-for-landings/9.webp)`,
                     backgroundSize: 'cover',
                 }}
             >
@@ -56,7 +56,7 @@ export const Banner = () => {
                             lineHeight: 1,
                             fontWeight: 600,
                             color: 'var(--main-color)',
-                            textShadow: '0px 4px 4px lightgray',
+                            textShadow: '2px 2px 2px white',
                             marginBottom: {xs: '5px', sm: '10px'}
                         }}>
                         Платная<br/>
